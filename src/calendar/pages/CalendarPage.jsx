2270d5f44c15b4a34eae130de975d727b1ec5f37
Diego Urrego <diegourrg@gmail.com>
2023-03-09 15:54:19 -0500

    Se crea cascarón inicial del proyecto. Se implementan rutas básicas y se empieza con la implementación del calendario.

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,37 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours } from 'date-fns';
+
+import { Navbar } from '../';
+import { getMessagesES, localizer } from '../../helpers';
+
+const events = [
+  {
+    title: 'Cumpleaños del jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'Diego',
+    },
+  },
+];
+
+export const CalendarPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 85px)' }}
+        messages={getMessagesES()}
+      />
+    </>
+  );
+};
