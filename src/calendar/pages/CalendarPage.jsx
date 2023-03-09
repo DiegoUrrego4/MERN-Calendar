31468c293406518e88ebebdff0b51c432402d91f
Diego Urrego <diegourrg@gmail.com>
2023-03-09 16:07:31 -0500

    Agregué nuevo componente para el manejo de información de los eventos. Se crea componente <CalendarEvent />.

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
 
-import { Navbar } from '../';
+import { CalendarEvent, Navbar } from '../';
 import { getMessagesES, localizer } from '../../helpers';
 
 const events = [
@@ -20,7 +20,6 @@ const events = [
 ];
 
 const eventStyleGetter = (event, start, end, isSelected) => {
-  console.log({ event, start, end, isSelected });
 
   const style = {
     backgroundColor: '#347CF7',
@@ -47,6 +46,9 @@ export const CalendarPage = () => {
         style={{ height: 'calc(100vh - 85px)' }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent
+        }}
       />
     </>
   );
